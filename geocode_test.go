539c36742f4c579cf6726eab1c110c25553a660b
Stefan Safranek <stefan@stefan>
2019-12-04 17:24:28 -0800

    cleaned up tests

--- geocode_test.go
+++ geocode_test.go
@@ -7,17 +7,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIP(t *testing.T) {
-	ip := "97.126.102.4"
-	location, err := IP(ip)
-	assert.Nil(t, err)
-	assert.Equal(t, "Seattle", location.City)
-	fmt.Printf("\n%+v\n", location)
-}
-
 func TestSearch(t *testing.T) {
 	search := "850 republican street, seattle"
-	location, err := Search(search)
+	location, err := Geocode(search)
 	assert.Nil(t, err)
 	assert.Equal(t, "Seattle", location.City)
 	fmt.Printf("\n%+v\n", location)
