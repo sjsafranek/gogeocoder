204de4127419875c5cdfac6daa162c4c264f7077
Stefan Safranek <stefan@stefan>
2019-12-05 10:14:07 -0800

    go fmt for zack

--- geocode.go
+++ geocode.go
@@ -52,25 +52,25 @@ func Geocode(search string) (Location, error) {
 }
 
 type Location struct {
-	PlaceID int     `json:"place_id,omitempty"`
-	OsmID	int	`json:"osm_id,omitempty"`
-	DisplayName string `json:"display_name,omitempty"`
-	Lat     string  `json:"lat,omitempty"`
-	Lon     string  `json:"lon,omitempty"`
-	Class     string  `json:"class,omitempty"`
-	Type     string  `json:"type,omitempty"`
-	Address Address `json:"address,omitempty"`
+	PlaceID     int      `json:"place_id,omitempty"`
+	OsmID       int      `json:"osm_id,omitempty"`
+	DisplayName string   `json:"display_name,omitempty"`
+	Lat         string   `json:"lat,omitempty"`
+	Lon         string   `json:"lon,omitempty"`
+	Class       string   `json:"class,omitempty"`
+	Type        string   `json:"type,omitempty"`
+	Address     Address  `json:"address,omitempty"`
 	BoundingBox []string `json:"boundingbox,omitempty"`
 }
 
 type Address struct {
-	Road string `json:"road,omitempty"`
-	Suburb string `json:"suburb,omitempty"`
-	PostCode string `json:"postcode,omitempty"`
-	Village string `json:"village,omitempty"`
-	City    string `json:"city,omitempty"`
-	County  string `json:"county,omitempty"`
-	State   string `json:"state,omitempty"`
-	Country string `json:"country,omitempty"`
+	Road        string `json:"road,omitempty"`
+	Suburb      string `json:"suburb,omitempty"`
+	PostCode    string `json:"postcode,omitempty"`
+	Village     string `json:"village,omitempty"`
+	City        string `json:"city,omitempty"`
+	County      string `json:"county,omitempty"`
+	State       string `json:"state,omitempty"`
+	Country     string `json:"country,omitempty"`
 	CountryCode string `json:"country_code,omitempty"`
 }
