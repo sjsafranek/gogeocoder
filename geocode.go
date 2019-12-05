87a310db11058e2853dd6df1267d9334468de4b5
Stefan Safranek <stefan@stefan>
2019-12-05 10:08:13 -0800

    expand address struct

--- geocode.go
+++ geocode.go
@@ -2,6 +2,7 @@ package gogeocoder
 
 import (
 	"encoding/json"
+	// "log"
 	"fmt"
 	"net/http"
 
@@ -12,6 +13,8 @@ func Reverse(longitude, latitude float64) (Location, error) {
 	var n Location
 
 	searchURL := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?format=json&addressdetails=1&lon=%v&lat=%v", longitude, latitude)
+
+	// log.Println(searchURL)
 
 	resp, err := http.Get(searchURL)
 	if err != nil {
@@ -34,6 +37,8 @@ func Geocode(search string) (Location, error) {
 
 	searchURL := fmt.Sprintf("https://nominatim.openstreetmap.org/search?q=%s&format=json&addressdetails=1", search)
 
+	// log.Println(searchURL)
+
 	resp, err := http.Get(searchURL)
 	if err != nil {
 		err = errors.Wrap(err, searchURL)
@@ -52,15 +57,24 @@ func Geocode(search string) (Location, error) {
 
 type Location struct {
 	PlaceID int     `json:"place_id,omitempty"`
+	OsmID	int	`json:"osm_id,omitempty"`
 	DisplayName string `json:"display_name,omitempty"`
 	Lat     string  `json:"lat,omitempty"`
 	Lon     string  `json:"lon,omitempty"`
+	Class     string  `json:"class,omitempty"`
+	Type     string  `json:"type,omitempty"`
 	Address Address `json:"address,omitempty"`
+	BoundingBox []string `json:"boundingbox,omitempty"`
 }
 
 type Address struct {
+	Road string `json:"road,omitempty"`
+	Suburb string `json:"suburb,omitempty"`
+	PostCode string `json:"postcode,omitempty"`
+	Village string `json:"village,omitempty"`
 	City    string `json:"city,omitempty"`
 	County  string `json:"county,omitempty"`
 	State   string `json:"state,omitempty"`
 	Country string `json:"country,omitempty"`
+	CountryCode string `json:"country_code,omitempty"`
 }
